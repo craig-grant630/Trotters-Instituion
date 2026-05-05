1b374e5657aeab34133ceede27fa278fc19c254a
nfb25161 <craig.grant.2025@uni.strath.ac.uk>
2026-05-05 18:55:41 +0100

    Add campus class to script

--- classes.py
+++ classes.py
@@ -1,3 +1,5 @@
+# https://dev.to/dpills/python-secure-password-management-hashing-and-encryption--1246 Encryption of passwords for later
+
 class Student:
     def __init__(self, student_id, name, programme_code, campus_code, year_of_study, password):
         self.student_id = student_id
@@ -9,3 +11,29 @@ class Student:
 
     def to_dict(self):
         return self.__dict__
+
+# Use class method in order to create new students from a dictionary
+    @classmethod
+    def from_dict(cls, data):
+        return cls(data['student_id'], data['name'], data['programme_code'], data['campus_code'],
+                   data['year_of_study'], data['password'])
+
+    def __repr__(self):
+        return f"Student ({self.student_id}), {self.name}"
+
+class Campus:
+    def __init__(self, campus_code, name):
+        self.campus_code = campus_code.upper()
+        self.name = name
+
+    def to_dict(self):
+        return self.__dict__
+
+    # Use class method in order to create new students from a dictionary
+    @classmethod
+    def from_dict(cls, data):
+        return cls(data['campus_code'], data['name'])
+
+    def __repr__(self):
+        return f"Campus ({self.campus_code}, {self.name})"
+
